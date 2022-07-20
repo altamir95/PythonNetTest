f32ec2157fd8a0063620f5cedb817097b22e4c06
altamir95 <altamir95@yandex.ru>
2022-07-20 20:47:01 +0300

    first

---
+++ PythonProject/PythonProject/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PythonProject;
+
+Console.WriteLine("Hello, World!");
+PythonOperation pop = new PythonOperation();
+pop.Initialize();
+pop.Execute();
+
---
+++ PythonProject/PythonProject/PythonOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Python.Runtime;
+namespace PythonProject
+{
+    public class PythonOperation
+    {
+        PyModule scope;
+        public PythonOperation()
+        {
+            //Runtime.PythonDLL= @"libpython3.10.dylib";
+        }
+
+        public void Initialize()
+        {
+            PythonEngine.Initialize();
+            scope = Py.CreateScope();
+        }
+
+        public void Execute()
+        {
+            using (Py.GIL())
+            {
+                scope.Exec("print('hello world')");
+            }
+        }
+    }
+}
+
